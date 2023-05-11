cefc12fc7fa5e5c905320d5a46963464f7fda283
haaniyairam <haaniyairam@gmail.com>
2023-05-11 19:53:37 +0530

    cool

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -17,7 +17,7 @@ contract WavePortal {
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves!", Patents);
+        console.log("We have %d Patents!", Patents);
         return totalWaves;
     }
 }
